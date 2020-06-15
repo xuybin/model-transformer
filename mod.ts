0443ddfaff9bb7d6e12b7856b966e8c3b74d992c
Xu YongBin <xuybin@qq.com>
2020-06-15 23:24:30 +0800

    [fix]

--- mod.ts
+++ mod.ts
@@ -1,7 +1,8 @@
-export { model } from "./model.ts";
 export { int } from "./validator/int.ts";
 export { float } from "./validator/float.ts";
 export { boolean } from "./validator/boolean.ts";
 export { string } from "./validator/string.ts";
 export { dateTime } from "./validator/dateTime.ts";
 export { json } from "./validator/json.ts";
+export { model } from "./model.ts";
+export { schema } from "./schema.ts";
