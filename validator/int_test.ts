e19d4bdbc9e10a2f44ec073862f9e140c7fdff21
Xu YongBin <xuybin@qq.com>
2020-06-16 12:05:13 +0800

    [fix]

--- validator/int_test.ts
+++ validator/int_test.ts
@@ -1,4 +1,7 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import {
+  assertEquals,
+  assertThrows,
+} from "https://deno.land/std/testing/asserts.ts";
 import { int } from "./int.ts";
 
 const { test } = Deno;
@@ -6,4 +9,18 @@ const { test } = Deno;
 test("int_type", () => {
   assertEquals(int().objectType, "number");
   assertEquals(int().fieldType, "int");
+  assertThrows(
+    () => {
+      int().default("abc");
+    },
+    Error,
+    "Expected to be called with 'default(*:number)",
+  );
+  assertThrows(
+    () => {
+      int().default(123).null;
+    },
+    Error,
+    "Expected to be called one of them 'null,default(*)'",
+  );
 });
